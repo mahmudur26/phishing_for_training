f19112d1e55e8f34e45778b805afdbc78a6ad29c
Aurko <mahmudur@appnap.io>
2024-12-10 22:13:24 +0600

    view update

--- resources/views/login_page.blade.php
+++ resources/views/login_page.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Facebook - Log In or Sign Up</title>
+  <title>Facebook | Log In or Sign Up</title>
   <style>
     /* General Reset */
 * {
@@ -63,7 +63,7 @@ body {
   gap: 15px;
 }
 
-input[type="email"], 
+input[type="email"],
 input[type="password"] {
   width: 100%;
   padding: 12px;
